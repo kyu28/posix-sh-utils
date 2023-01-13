fe78bb60e5f2532bf8d2e2f01949a86c0a89d39b
KilobY73 <66195452+KilobY73@users.noreply.github.com>
2023-01-13 12:03:35 +0800

    Bug fixes

--- sfm.sh
+++ sfm.sh
@@ -42,6 +42,7 @@ sfm_update() {
   sfm_files_num=$#
   if [ $(($sfm_start + $sfm_cursor - 1)) -gt $sfm_files_num ]; then
     sfm_cursor=$(($sfm_height - 1))
+    [ $sfm_cursor -gt $sfm_files_num ] && sfm_cursor=$sfm_files_num
     sfm_start=$(($sfm_files_num - $sfm_cursor + 1))
   fi
   sfm_i=1
@@ -113,11 +114,8 @@ sfm_key_input() {
       read -rn 1 sfm_input
       if [ $sfm_input = 'y' ]; then
         rm -rf "$sfm_cur_file"
-        if [ $? -eq 0 ]; then
-          sfm_update
-        else
-          printf "\033[$sfm_height;1H\033[2K$sfm_error" && sleep 3
-        fi
+        [ $? -ne 0 ] && sleep 3
+        sfm_update
       fi;;
     'r')
       printf "\033[$sfm_height;1H\033[2KNew path: \033[?25h"
@@ -127,11 +125,8 @@ sfm_key_input() {
       printf "\033[?25l"
       if [ -n "$sfm_input" ]; then
         mv "$sfm_cur_file" "$sfm_input"
-        if [ $? -eq 0 ]; then
-          sfm_update
-        else
-          printf "\033[$sfm_height;1H\033[2K$sfm_error" && sleep 3
-        fi
+        [ $? -ne 0 ] && sleep 3
+        sfm_update
       fi;;
     ' ')
       sfm_marked_flag=0
@@ -146,21 +141,29 @@ sfm_key_input() {
         printf "\033[$sfm_cursor;1H\033[7m  $sfm_cur_file\n\033[m"
       fi;;
     'v')
-      mv $sfm_marked .
-      sfm_marked=""
-      sfm_update;;
-    'p')
-      /bin/cp -rf $sfm_marked . # Careful with *
-      sfm_marked=""
-      sfm_update;;
-    'd')
-      printf "\033[$sfm_height;1H\033[2KDelete these files? (y/N)"
-      read -rn 1 sfm_input
-      if [ $sfm_input = 'y' ]; then
-        rm -rf $sfm_marked
+      if [ -n "$sfm_marked" ]; then
+        mv $sfm_marked .
         [ $? -ne 0 ] && sleep 3
         sfm_marked=""
         sfm_update
+      fi;;
+    'p')
+      if [ -n "$sfm_marked" ]; then
+        /bin/cp -rf $sfm_marked . # Careful with *
+        [ $? -ne 0 ] && sleep 3
+        sfm_marked=""
+        sfm_update
+      fi;;
+    'd')
+      if [ -n "$sfm_marked" ]; then
+        printf "\033[$sfm_height;1H\033[2KDelete these files? (y/N)"
+        read -rn 1 sfm_input
+        if [ $sfm_input = 'y' ]; then
+          rm -rf $sfm_marked
+          [ $? -ne 0 ] && sleep 3
+          sfm_marked=""
+          sfm_update
+        fi
       fi;;
     'm')
       printf "\033[$sfm_height;1H\033[2KNew directory name: \033[?25h"
@@ -170,11 +173,8 @@ sfm_key_input() {
       printf "\033[?25l"
       if [ -n "$sfm_input" ]; then
         mkdir "$sfm_input"
-        if [ $? -eq 0 ]; then
-          sfm_update
-        else
-          printf "\033[$sfm_height;1H\033[2K$sfm_error" && sleep 3
-        fi
+        [ $? -ne 0 ] && sleep 3
+        sfm_update
       fi;;
   esac
 }
