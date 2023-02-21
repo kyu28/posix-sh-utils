288d65e8cb981b4504ead96f077f8061d4fdf183
kyu28 <66195452+kyu28@users.noreply.github.com>
2023-02-21 09:12:32 +0800

    Delete perfset_cpupower.sh

--- perfset/perfset_cpupower.sh
+++
@@ -1,9 +0,0 @@
-mode=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
-if [ "$mode" = "performance" ]
-then
-	sudo cpupower frequency-set -g powersave
-	xbacklight -dec 15
-else
-	sudo cpupower frequency-set -g performance
-	xbacklight -inc 15
-fi
